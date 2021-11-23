c024c404abf6aae4596fb19e290153b9562135cd
Scala Steward <me@scala-steward.org>
2021-11-23 01:08:48 +0100

    scala-library 2.13.7 (was 2.13.6)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val Scala210 = "2.10.7"
   val Scala211 = "2.11.12"
   val Scala212 = "2.12.15"
-  val Scala213 = "2.13.6"
+  val Scala213 = "2.13.7"
   val Scala3   = "3.0.2"
 
   val ScalaVersions = Seq(Scala210, Scala211, Scala212, Scala213, Scala3)
