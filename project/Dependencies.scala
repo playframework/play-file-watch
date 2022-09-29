6e1395107520080e19cf39a1183b7f19161c98f9
Matthias Kurz <m.kurz@irregular.at>
2022-09-29 12:53:23 +0200

    Drop Scala 2.10 + 2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,12 +4,9 @@
 import sbt._
 
 object Dependencies {
-  // to allow for use in sbt 0.13
-  val Scala210 = "2.10.7"
-  val Scala211 = "2.11.12"
   val Scala212 = "2.12.16"
   val Scala213 = "2.13.8"
   val Scala3   = "3.2.0"
 
-  val ScalaVersions = Seq(Scala210, Scala211, Scala212, Scala213, Scala3)
+  val ScalaVersions = Seq(Scala212, Scala213, Scala3)
 }
